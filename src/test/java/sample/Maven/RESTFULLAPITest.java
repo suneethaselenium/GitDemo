0dba6b3bb7398efae88999c827eb5fee14ec737b
suneethaselenium <suniseeram@yahoo.com>
2021-03-07 20:17:04 -0600

    first commit

---
+++ src/test/java/sample/Maven/RESTFULLAPITest.java
@@ -0,0 +1,17 @@
+package sample.Maven;
+
+import org.junit.Test;
+
+public class RESTFULLAPITest {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");	
+	}
+}
---
+++ src/test/java/sample/Maven/SeleniumTest.java
@@ -0,0 +1,18 @@
+package sample.Maven;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+
+@Test
+public void BrowserAUtomation()
+{
+	System.out.println("BrowserAUtomation");	
+}
+@Test
+public void elementsUi()
+{
+	System.out.println("elementsUi");	
+}
+
+}
