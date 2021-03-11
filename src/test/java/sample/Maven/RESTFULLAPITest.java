6e64ff96e8c5845d66ec904e2e5086b8346bbcea
suneethaselenium <suniseeram@yahoo.com>
2021-03-10 19:21:29 -0600

    commit from gitDemo

--- src/test/java/sample/Maven/RESTFULLAPITest.java
+++ src/test/java/sample/Maven/RESTFULLAPITest.java
@@ -9,9 +9,6 @@ public class RESTFULLAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira1");
 		System.out.println("postJira2");
-		System.out.println("postJira3");
-		System.out.println("postJira4");
-		System.out.println("postJira5");
 		
 	}
 	
